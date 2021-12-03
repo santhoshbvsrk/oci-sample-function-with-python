f5ce9fd4b27f7f8739d6c5d384ce6c471fef8f27
fahdabidiroottenancy/Rahul <test@example.com>
2021-12-03 12:03:49 +0530

    Added custm docker file

--- fn-with-customImage/func.py
+++ fn-with-customImage/func.py
@@ -1,6 +1,3 @@
-#
-# oci-load-file-into-adw-python version 1.0.
-#
 # Copyright (c) 2020 Oracle, Inc.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 #
